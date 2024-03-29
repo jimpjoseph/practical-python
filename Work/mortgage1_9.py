308ff0b0b1dc3406fba611005db4312050442a0d
James Joseph <jim.p.joseph@gmail.com>
2024-03-28 22:50:05 -0500

    Ex 1.9

---
+++ Work/mortgage1_9.py
@@ -0,0 +1,21 @@
+# mortgage.py
+#
+# Exercise 1.8
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+month_counter = 0 
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000.0
+
+while principal > 0:
+	principal = principal * (1 + rate /12) - payment
+	total_paid = total_paid + payment
+	if month_counter >= extra_payment_start_month and month_counter < extra_payment_end_month:
+		principal = principal - extra_payment
+		total_paid = total_paid + extra_payment
+	month_counter = month_counter + 1
+
+print('Total Paid', round(total_paid,2))
