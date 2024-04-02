58d806ae0e2b1c269cc065c201616859fd8235c5
James Joseph <jim.p.joseph@gmail.com>
2024-04-01 22:37:26 -0500

    3.9

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -15,9 +15,11 @@ def parse_csv(filename: str, select = None, types = None, has_headers = True, de
 		if select and not has_headers:
 			raise RuntimeError("select argument requires column headers")
 
+		start_no = 0
 		#Read the file headers
 		if has_headers:
 			headers = next(rows)
+			start_no = 1
 
 		if select:
 			indices = [headers.index(colname) for colname in select]
@@ -26,20 +28,27 @@ def parse_csv(filename: str, select = None, types = None, has_headers = True, de
 			indices = []
 
 		records = []
-		for row in rows:
+
+		for lineno, row in enumerate(rows, start = start_no):
 			if not row: 	#Skip rows with no data
 				continue
-			if types:
-				row = [func(val) for func, val in zip(types, row)]
+			try:
+				if types:
+					row = [func(val) for func, val in zip(types, row)]
 
-			if indices:
-				row = [ row[index] for index in indices]
+				if indices:
+					row = [ row[index] for index in indices]
 
-			if has_headers:
-				record = dict(zip(headers, row))
-				records.append(record)
-			else:
-				records.append(tuple(row))
+				if has_headers:
+					record = dict(zip(headers, row))
+					records.append(record)
+				else:
+					records.append(tuple(row))
+			
+			except ValueError as e:
+				print(f"Row {lineno}: Couldn't covert {row}")
+				print(f"Row {lineno}: Reason {e}")
+
 
 	return records
 
