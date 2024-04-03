8be711decfeb52829981ed0d6de87abcfa6d3be3
James Joseph <jim.p.joseph@gmail.com>
2024-04-02 22:33:59 -0500

    3.4

---
+++ Work/pcost_3_14.py
@@ -0,0 +1,17 @@
+# pcost.py
+#
+# Exercise 1.33
+import sys
+from report_3_12 import read_portfolio
+
+if len(sys.argv) == 2:
+	filename = sys.argv[1]
+else:
+	filename = 'Data/portfolio.csv'
+
+cost = 0.0
+portfolio = read_portfolio(filename)
+for p in portfolio:
+	cost += p['shares'] * p['price'] 
+
+print('Total cost ',f'{cost:10.2f}')
